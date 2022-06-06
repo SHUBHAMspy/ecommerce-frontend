d0423a52b52838df17d5c6a787ddf317cd5b1142
Shubham Pandey <shubhampandey1964@gmail.com>
2022-06-06 16:53:51 +0530

    Established Pagination

--- src/components/pagination/PaginationComponent.jsx
+++ src/components/pagination/PaginationComponent.jsx
@@ -1,37 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 const Pagination = ({pageCount,changePage}) => {
+  const [currentPage, setCurrentPage] = useState(1)
+  
   return (
-    <div className="container pages">
+    <div className="pages">
+      <button 
+        className={`paginationBttns chip ${currentPage === 1 ? 'disable': ''}`} 
+        onClick={() => {
+          changePage(currentPage - 1);
+          setCurrentPage(currentPage - 1);
+        }}
+      > &lt; </button>
       {
         [...Array(pageCount).keys()].map(value => {
+            const active = (value + 1 === currentPage) ? 'activePage': ''
             return (
               <button 
-                onClick={() => changePage(value+1)}
+                onClick={() => {
+                  changePage(value+1)
+                  setCurrentPage(value + 1);
+                }}
                 key={value} 
-                className="paginationBttns chip"
+                className={`paginationBttns chip ${active}`}
               >
                 {value+1}
               </button>
             )
         })
       }
+
+      <button 
+        className={`paginationBttns chip ${currentPage === pageCount ? 'disable': ''}`} 
+        onClick={() => {
+        changePage(currentPage + 1);
+        setCurrentPage(currentPage + 1);
+        }}
+      
+      >
+        &gt; </button>
     
     </div>
-    // <>
-    //   <ReactPaginate
-    //     previousLabel={"<"}
-    //     nextLabel={">"}
-    //     pageCount={pageCount}
-    //     onPageChange={changePage}
-    //     containerClassName={"paginationBttns"}
-    //     previousLinkClassName={"previousBttn"}
-    //     nextLinkClassName={"nextBttn"}
-    //     disabledClassName={"paginationDisabled"}
-    //     activeClassName={"paginationActive"}
-    //   />
-      
-    // </>
+    
   )
 }
 
